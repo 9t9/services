95e3508e914f1f67967ea04188ecf51e883e0301
Usman Tahir <usman.tahir@blueeast.com>
2017-03-08 17:30:21 +0500

    removed notifications

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.Notifications = exports.withNotifications = exports.reducers = exports.withSystemNav = exports.App = undefined;
+exports.withNotifications = exports.reducers = exports.withSystemNav = exports.App = undefined;
 
 var _withSystemNav = require('./SystemNav/withSystemNav');
 
@@ -21,14 +21,9 @@ var _withNotifications = require('./Notifications/withNotifications');
 
 var _withNotifications2 = _interopRequireDefault(_withNotifications);
 
-var _Notifications = require('./Notifications');
-
-var _Notifications2 = _interopRequireDefault(_Notifications);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.App = _App2.default;
 exports.withSystemNav = _withSystemNav2.default;
 exports.reducers = _reducers2.default;
 exports.withNotifications = _withNotifications2.default;
-exports.Notifications = _Notifications2.default;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import withSystemNav from './SystemNav/withSystemNav';
 import reducers from './reducers';
 import App from './App';
 import withNotifications from './Notifications/withNotifications';
-import Notifications from './Notifications';
 
 export {
   App,
   withSystemNav,
   reducers,
-  withNotifications,
-  Notifications
+  withNotifications
 };
