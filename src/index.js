a717df266c0c24750fe40e0bad4d417888772157
Usman Tahir <usman.tahir@blueeast.com>
2017-03-08 17:24:08 +0500

    Notification added to exports

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import withSystemNav from './SystemNav/withSystemNav';
 import reducers from './reducers';
 import App from './App';
 import withNotifications from './Notifications/withNotifications';
+import Notifications from './Notifications';
 
 export {
   App,
   withSystemNav,
   reducers,
   withNotifications,
+  Notifications
 };
