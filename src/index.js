cf2a6dbe7cb54658420b36d808c9b2d5e4a76260
Usman Tahir <usman.tahir@blueeast.com>
2017-03-08 15:45:05 +0500

    removed post install

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import withSystemNav from './SystemNav/withSystemNav';
 import reducers from './reducers';
 import App from './App';
+import withNotifications from './Notifications/withNotifications';
 
 export default {
   App,
   withSystemNav,
   reducers,
+  withNotifications,
 };
